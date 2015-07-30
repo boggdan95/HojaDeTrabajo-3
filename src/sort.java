107a971d7b2ecee94d813e1f115d48277ff617c3
boggdan95 <bandabog@gmail.com>
2015-07-30 13:41:44 +0000

    Iniciar creacion de archivo

--- src/sort.java
+++ src/sort.java
@@ -1,11 +1,18 @@
+import java.io.File;
+
 /**
  * 
  */
 
 /**
- * @author Alicia-Lester
+ * @author Boggdan Barrientos, Jonathan Aguirre, Jose Corona, Julio Dieguez 
  *
  */
 public class sort {
+	
+	public void generarArchivo(File archivo){
+		String numeros = "numeros.txt";
+		File fichero = new File (numeros);
+	}
 
 }
