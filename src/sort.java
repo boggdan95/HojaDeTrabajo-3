3d18c0f7f16fdd684c6113e7778f2e0b2405014e
boggdan95 <bandabog@gmail.com>
2015-07-30 13:53:51 +0000

    Arreglar

--- src/sort.java
+++ src/sort.java
@@ -1,4 +1,7 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * 
@@ -13,6 +16,22 @@ public class sort {
 	public void generarArchivo(File archivo){
 		String numeros = "numeros.txt";
 		File fichero = new File (numeros);
+		
+		if (fichero.exists()){
+			try {
+				BufferedWriter escritor = new BufferedWriter(new FileWriter(numeros));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		for (int i = 0; i < 3000; i++){
+			int num = 0;
+			num = Math.subtractExact(0, 3000);
+			escritor.write(" "+num+" ");
+			
+		}	
+		
+		}
 	}
 
 }
